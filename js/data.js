5a8407912ca251de5f949003298a27996e31af30
j.deymann <joerg.deymann@die-deymanns.de>
2024-06-21 13:39:08 +0200

    put data seperate

---
+++ js/data.js
@@ -0,0 +1,20 @@
+catlist=[
+    {
+        "catname": "Harry",
+        "img":"./img/harry.jpg",
+        "text":`
+            Harry ist ein gemütlicher Kater, der gerne Nachts auf Tour geht. Wenn er zuhause ist möchte er gerne an seinem 
+            Bauch und dem Kopf gestreichelt werden. Wenn Harry will, kann er auch sehr melodisch sein.  Sein Name hat er aus "Harry Potter".
+        `
+    },
+    {
+        "catname": "Molly",
+        "img":"./img/molly.jpg",
+        "text":`
+            Molly ist eine sehr aktive weibliche Katze, spielt gerne mit Blättern. Sie lässt sich nur selten auf die Arme nehmen, und wenn dann ganz kurz. 
+            Sie mag lieber am Boden gestreichelt werden, und fordert, daß man mit ihr spielt.
+            Sie hat ihren Namen aus "Harry Potter".
+        `
+    }        
+];
+
